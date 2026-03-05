7001b3d048395b74a946ae9daa17aa403e87e4bd
jjackson112 <jjackson11211@gmail.com>
2026-03-05 15:58:42 -0500

    DB setup

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# create the database without the app and initialize later
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+# Database configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///polynotes.db"
+
+
