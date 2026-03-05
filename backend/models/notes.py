a723a4c89912de21599debafe9fae7358c973bc4
jjackson112 <jjackson11211@gmail.com>
2026-03-05 16:54:28 -0500

    change how username is stored - data duplication + notes not to be unique by title + auto update for updated_at

--- backend/models/notes.py
+++ backend/models/notes.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), unique=True, nullable=False)
-    title = db.Column(db.String(50), unique=False, nullable=False)
-    content = db.Column(db.String(1000), unique=False, nullable=False)
-    language = db.Column(db.String(50), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user_id"), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    language = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def to_dict(self):
         return {
