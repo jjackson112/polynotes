01c0f133572aeeec611b2a06914bb5d596f8b1a9
jjackson112 <jjackson11211@gmail.com>
2026-03-05 18:39:57 -0500

    separate GET + POST endpoints

--- backend/routes/notes.py
+++ backend/routes/notes.py
@@ -3,17 +3,38 @@
 
 from flask import Blueprint, request, jsonify
 from app import db
+from models.notes import Note
 
-note_bp = Blueprint("note", __name__, url_prefix='/api/note')
+notes_bp = Blueprint("notes", __name__, url_prefix='/api/notes')
 
-@note_bp.route("/", methods=["GET"])
+@notes_bp.route("/<int:user_id>", methods=["GET"])
+def get_note(user_id):
+    language = request.args.get("language")
+    tag = request.args.get("tags")
+    
+    query = Note.query.filter_by(user_id=user_id)
+    
+    if language:
+        query = query.query.filter_by(language=language)
+
+    if tag:
+        query =Note.query.join(Note.tags).filter(Tag.name ==tag)
+    
+    notes = query.all()
+
+    results =[]
+
+    return jsonify({"Success"}), 200
+
+
+@notes_bp.route("/", methods=["POST"])
 def create_note():
     data = request.get_json()
 
     if not data:
         return jsonify({"Invalid JSON"}), 400
     
-    db.session.add(note)
+    db.session.add()
     db.session.commit()
-
-    return jsonify({"Success"}), 200
+    
+    return jsonify({"Success"}), 201
