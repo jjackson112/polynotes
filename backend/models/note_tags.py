4ddbcd0d680f779ab8d528548343215b41faed6c
jjackson112 <jjackson11211@gmail.com>
2026-03-06 17:01:41 -0500

    update note + tag models

--- backend/models/note_tags.py
+++ backend/models/note_tags.py
@@ -1,5 +1,6 @@
 from app import db
 
+# association or junction table 
 note_tags = db.Table(
     "note_tags",
     db.Column("note_id", db.Integer, db.ForeignKey("note.id")),
--- backend/models/tags.py
+++ backend/models/tags.py
@@ -1,9 +1,10 @@
 from app import db
+from models.note_tags import note_tags
 
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-
+    notes = db.relationship("Note", secondary=note_tags, back_populates="tags")
     def to_dict(self):
         return {
             "id": self.id,
