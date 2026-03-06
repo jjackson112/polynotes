faf2b064b8698c6218d37869a106765c92d9ab1e
jjackson112 <jjackson11211@gmail.com>
2026-03-05 19:13:32 -0500

    for loop through each note

--- backend/routes/notes.py
+++ backend/routes/notes.py
@@ -4,6 +4,7 @@
 from flask import Blueprint, request, jsonify
 from app import db
 from models.notes import Note
+from models.tags import Tag
 
 notes_bp = Blueprint("notes", __name__, url_prefix='/api/notes')
 
@@ -15,16 +16,25 @@ def get_note(user_id):
     query = Note.query.filter_by(user_id=user_id)
     
     if language:
-        query = query.query.filter_by(language=language)
+        query = query.filter_by(language=language)
 
     if tag:
-        query =Note.query.join(Note.tags).filter(Tag.name ==tag)
+        query =Note.query.join(Note.tags).filter(Tag.name == tag)
     
-    notes = query.all()
+    notes = query.all() # list of SQLAlchemy objects not JSON
 
     results =[]
 
-    return jsonify({"Success"}), 200
+    # objects → dictionaries → Flask converts to JSON
+    for note in notes:
+        results.append({
+            "id": note.id,
+            "title": note.title,
+            "content": note.content,
+            "language": note.language
+        })
+
+    return jsonify(results), 200
 
 
 @notes_bp.route("/", methods=["POST"])
@@ -37,4 +47,4 @@ def create_note():
     db.session.add()
     db.session.commit()
     
-    return jsonify({"Success"}), 201
+    return jsonify(), 200
