1d5e8336f1544bd490ea670694c3b417d1f9e092
jjackson112 <jjackson11211@gmail.com>
2026-03-07 20:43:51 -0500

    decorator - token_required

--- backend/services/token.py
+++ backend/services/token.py
@@ -9,7 +9,7 @@ from models.user import User
 # only the server knows the secret key
 
 # when user wants to see the token the frontend sends that token to the header
-# decorator (token_required) - decodes the token, verifies tehe signature, and pulls the user_id and attaches the User object to the request
+# decorator (token_required) - decodes the token, verifies the signature, and pulls the user_id and attaches the User object to the request
 
 def token_required(f):
     @wraps(f)
@@ -18,8 +18,17 @@ def token_required(f):
 
         # Authorization header tells system it's a JWT
         if 'Authorization' in request.headers:
-            auth_header = request.headers:
+            auth_header = request.headers['Authorization']
             token = auth_header.split(" ")[1] if " " in auth_header else auth_header
 
         if not token:
             return jsonify({'error', 'Token is missing'}), 401
+        
+        try:
+            data = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
+            current_user = User.query.get(data['user_id'])
+        except:
+            return jsonify({'error': 'Token is invalid or expired!'}), 401
+
+        return f(current_user, *args, **kwargs)
+    return decorated
