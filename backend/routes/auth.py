311e751848d4e6750382bddf9d33abf01d436969
jjackson112 <jjackson11211@gmail.com>
2026-03-07 22:31:28 -0500

    generate JWT token + registration endpoint

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -16,5 +16,20 @@ def login():
     if not user or not user.check_password(password):
         return jsonify({'error': "Invalid username or password"}), 401
 
+    # Generate JWT token
+    token = jwt.encode({
+        'user_id': user.id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)},
+        os.getenv('SECRET_KEY'), algorithm='HS256')
+    
+    return jsonify({'token': token
+    })
+
 @notes_bp.route("/register", methods=["POST"])
 def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or password:
+        return({'error': "Username and password are required"}), 400
