f45893be50608cb8f6aa5daafd5c9557def38ce5
jjackson112 <jjackson11211@gmail.com>
2026-03-07 22:24:27 -0500

    start login endpoint

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,17 +2,19 @@
 # Login route verifies credentials + returns the JWT
 # Register route hashes the password + saves the new user to the database
 import jwt
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
+from flask import request, jsonify
+import datetime
 from models.user import User
-from flask_jwt_extended import JWTManager
-
-notes_bp = Blueprint("notes", __name__, url_prefix='/api/notes')
 
 @notes_bp.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.check_password(password):
+        return jsonify({'error': "Invalid username or password"}), 401
+
+@notes_bp.route("/register", methods=["POST"])
+def register():
