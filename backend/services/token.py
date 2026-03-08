f56a67c9837b470795bc9c37ab1a828dc3d0fef8
jjackson112 <jjackson11211@gmail.com>
2026-03-07 20:39:11 -0500

    auth header

--- backend/services/token.py
+++ backend/services/token.py
@@ -4,10 +4,22 @@ import jwt
 from models.user import User
 
 # JWT is stateless - not stored in db - has 3 parts
-# Header tells system it's a JWT
 # Payload is the data - user_id, etc.
 # Signature - created by taking the header + payload + secret key and hashing them
 # only the server knows the secret key
 
 # when user wants to see the token the frontend sends that token to the header
 # decorator (token_required) - decodes the token, verifies tehe signature, and pulls the user_id and attaches the User object to the request
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        # Authorization header tells system it's a JWT
+        if 'Authorization' in request.headers:
+            auth_header = request.headers:
+            token = auth_header.split(" ")[1] if " " in auth_header else auth_header
+
+        if not token:
+            return jsonify({'error', 'Token is missing'}), 401
