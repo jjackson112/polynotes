e65978e409d412dcde74063755cbbc5f77ec37f3
jjackson112 <jjackson11211@gmail.com>
2026-03-17 17:17:24 -0400

    include all required fields in validation.py

--- backend/validation/note_validation.py
+++ backend/validation/note_validation.py
@@ -1,7 +1,17 @@
-# helper method for activity
+# helper method for activity 
+# catch bad data before it hits the db
 
 def require_fields(data):
     if not data:
-        return False, "Invalid JSON"
+        return "Invalid JSON"
     
+    if not data.get("title"):
+        return "Title is required"
     
+    if not data.get("content"):
+        return "Content is required"
+    
+    if not data.get("language"):
+        return "Selecting a language is required"
+
+    return None
