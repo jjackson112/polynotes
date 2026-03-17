5af39a3ca79d7033d73580cc1bb3cfdc6d412208
jjackson112 <jjackson11211@gmail.com>
2026-03-17 16:56:00 -0400

    new tags

--- backend/services/tag_service.py
+++ backend/services/tag_service.py
@@ -8,4 +8,12 @@ from models.tags import Tag
 existing_tags = Tag.query.filter_by().all()
 existing_names = {tag.name for tag in existing_tags}
 
+new_tags = []
 
+for name in tag_names:
+    if name not in existing_names:
+        tag = Tag(name=name)
+        db.session.add(tag)
+        new_tags.append(tag)
+
+    return existing_tags + new_tags
