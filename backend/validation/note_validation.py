41f680acf975147ae82f88431e6faa5061540313
jjackson112 <jjackson11211@gmail.com>
2026-03-17 22:19:45 -0400

    update validation - PATCH - only validate what is being provided

--- backend/validation/note_validation.py
+++ backend/validation/note_validation.py
@@ -1,5 +1,5 @@
-# helper method for activity 
-# catch bad data before it hits the db
+# helper method for activity + catch bad data before it hits the db
+# split validation for POST + PATCH
 
 def require_fields(data):
     if not data:
@@ -20,3 +20,13 @@ def require_fields(data):
         return "Invalid language"  
 
     return None
+
+def validate_update_note(data):
+    if not data:
+        return "Invalid JSON"
+    
+    allowed_fields = ["title", "content", "language", "tags"]
+
+    for key in data:
+        if key not in allowed_fields:
+            return f"Invalid field: {key}"
