f7125c00afad5e2d42b3847eebb2229f059e16e2
jjackson112 <jjackson11211@gmail.com>
2026-04-09 19:13:53 -0400

    separate testing routes for pytest

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,12 @@
 # turn on testing mode + use an in-memory db (fresh every test run)
 # yields back control to pytest
+# client (HTTP - ThunderClient)
+# db - clean db per test
+# auth_header - (logged-in token helper)
 
 import pytest
-from app import create_app 
+from app import create_app
+from extensions import db
 
 @pytest.fixture
 def app():
@@ -14,4 +18,34 @@ def app():
         "WTF_CSRF_ENABLED": False,
     })
 
-    yield app
+    with app.app_context():
+        db.create_all()
+        yield db
+        db.drop_all()
+
+# test client fixture
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+# auth helper texture
+# no more repeating register/login for each test
+@pytest.fixture
+def new_user(client):
+    client.post("/api/register", json={
+        "email": "jazz@example.com",
+        "username": "jazz",
+        "password": "password123"
+    })
+
+    login_res = client.post("/api/login", json={
+        "username": "jazz",
+        "password": "password123"
+    })
+
+    token = login_res.json["token"]
+
+    return {
+        "token": token,
+        "headers": {"Authorization": f"Bearer {token}"}
+    }
--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,49 +1,8 @@
-# client (HTTP - ThunderClient)
-# db - clean db per test
-# auth_header - (logged-in token helper)
+# SoC for testing routes
 
-import pytest
-from app import create_app
-from extensions import db
-
-@pytest.fixture
-def client(create_app):
-    return create_app.test_client()
-
-# test client fixture
 def test_health(client):
     res = client.get("/api/health")
     assert res.status_code == 200
-
-# db fixture
-@pytest.fixture
-def db_session(create_app):
-    with app.app_context():
-        db.create_all()
-        yield db
-        db.drop_all()
-
-# auth helper texture
-# no more repeating register/login for each test
-@pytest.fixture
-def new_user(client):
-    client.post("/api/register", json={
-        "email": "jazz@example.com",
-        "username": "jazz",
-        "password": "password123"
-    })
-
-    login_res = client.post("/api/login", json={
-        "username": "jazz",
-        "password": "password123"
-    })
-
-    token = login_res.json["token"]
-
-    return {
-        "token": token,
-        "headers": {"Authorization": f"Bearer {token}"}
-    }
 
 def test_protected_route(client, new_user):
     res = client.get("/api/protected", headers=new_user["headers"])
