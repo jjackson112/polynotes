e32caf185f94cb393f9e15fab90ed2e7c52e1b9f
jjackson112 <jjackson11211@gmail.com>
2026-04-09 20:03:21 -0400

    if the token fails

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -7,3 +7,7 @@ def test_health(client):
 def test_protected_route(client, new_user):
     res = client.get("/api/protected", headers=new_user["headers"])
     assert res.status_code == 200
+
+def test_protected_no_token(client):
+    res = client.get("/api/protected")
+    assert res.status_code == 401
