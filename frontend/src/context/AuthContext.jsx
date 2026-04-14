e9a2cbf89a59ea4263b54960cb989b884de6ef6e
jjackson112 <jjackson11211@gmail.com>
2026-04-13 20:16:06 -0400

    restore session on page load + login + logout from useAuth.js moved to AuthContext.jsx - global state management

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,3 +2,37 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 // create context
 const AuthContext = createContext()
+
+// provider component
+export function AuthProvider({ children }) {
+    const [token, setToken] = useState(null)
+
+    // restore session on load
+    useEffect(() => {
+        const storedToken = localStorage.getItem("token")
+
+        if (storedToken) {
+            setToken(storedToken)
+        }
+    }, [])
+
+    // login function
+    const login = (newToken) => {
+        localStorage.setItem("token", newToken)
+        setToken(newToken)
+    }
+    
+    // logout function
+    const logout = () => {
+        localStorage.removeItem("token")
+        setToken(null)
+    } 
+
+    const userLoggedIn = !!token;
+
+    return (
+        <AuthContext.Provider value={{ token, login, logout, userLoggedIn }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
