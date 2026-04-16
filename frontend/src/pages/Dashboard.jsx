73e588f7c759b3b8fe756a7c2176b6abaa354109
jjackson112 <jjackson11211@gmail.com>
2026-04-16 15:28:22 -0400

    useEffect for APIs - handles the response

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,19 @@
 import { useEffect } from "react";
+import { api } from "../api/api";
 
 // /dashboard  → ProtectedRoute  → Dashboard renders
 // protected logic will live here - GET requests
 function Dashboard() {
     useEffect(() => {
-        api.get("/auth/protected")
+        const fetchData = async() => {
+            try {
+                const request = await api.get("/auth/protected")  
+                console.log(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchData()
     }, [])
 
     return <h1>Dashboard (Protected) </h1>
