0ccbfd8a39922631703e7e90365a1d7008bbf2f2
jjackson112 <jjackson11211@gmail.com>
2026-04-16 18:23:08 -0400

    what happens after registering - login + navigate to dashboard

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { api } from "../api/api";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
     const [registerForm, setRegisterForm] = useState({
@@ -8,12 +9,18 @@ function Register() {
         password: ""
     })
 
+    const navigate = useNavigate()
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log("Successful register")
 
         try {
             const data = await api.post("/auth/register", registerForm)
+            console.log("Successful register")
+
+            // centralized auth replacing stored token + updating login state - successful login
+            login(data.token)
+            navigate("/dashboard")
 
         } catch (err) {
             console.error("Network error:", err)
