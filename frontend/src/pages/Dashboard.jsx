21bec8c2e4773f60340ae278e64cb7b46efe7a5d
jjackson112 <jjackson11211@gmail.com>
2026-04-16 15:15:16 -0400

    protected logic moves to Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,11 @@
+import { useEffect } from "react";
+
 // /dashboard  → ProtectedRoute  → Dashboard renders
 function Dashboard() {
+    useEffect(() => {
+        api.get("/auth/protected")
+    }, [])
+
     return <h1>Dashboard (Protected) </h1>
 }
 
