9d5ef0960add6290d185671f99cca9d22f55ad82
jjackson112 <jjackson11211@gmail.com>
2026-04-30 16:59:55 -0400

    api layer turned into a factory function - API depends on token

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,43 +1,35 @@
 // central API layer - just call it from here
 // API layer separately looks into localStorage
+// inject token from context - make API depend on token
+// exporting a static object > move API into a factory function
 
-const BASE_URL = "http://localhost:5000/api";
+export const createApi = (token) => {
+  const BASE_URL = "http://localhost:5000/api";
 
-export const api = {
-  get: async (endpoint) => {
-    const token = localStorage.getItem("token");
+  const headers = {
+    "Content-Type": "application/json",
+    ...BASE_URL(token && { Authorization: `Bearer ${token}`})
+  }
 
-    const res = await fetch(`${BASE_URL}${endpoint}`, {
-      headers: { 
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-      }
-    });
+  return {
+    get: async (endpoint) => {
+      const res = fetch(`${BASE_URL}${endpoint}`, {
+        headers
+      })
 
-    if (!res.ok) throw new Error(`GET failed: ${res.status}`)
+      if (!res.ok) throw new Error(`GET failed: ${res.status}`)
+      return res.json()
+    },
+  
+    post: async(endpoint, body) => {
+      const res = await fetch(`${BASE_URL}${endpoint}`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body)
+      })
 
-    return res.json();
-  },
-
-  post: async (endpoint, body) => {
-    const token = localStorage.getItem("token");
-
-    const headers = {
-      "Content-Type": "application/json",
-    };
-
-    if (token) {
-      headers.Authorization = `Bearer ${token}`;
+      if (!res.ok) throw new Error(`POST failed`)
+      return res.json()
     }
-
-    const res = await fetch(`${BASE_URL}${endpoint}`, {
-      method: "POST",
-      headers,
-      body: JSON.stringify(body),
-    });
-
-    if (!res.ok) throw new Error("POST request failed");
-
-    return res.json();
-  },
-};
+  }
+}
