90dc69e51084d74fb19b46cf697055416bcde8c1
jjackson112 <jjackson11211@gmail.com>
2026-05-11 16:56:34 -0400

    Modal btn CSS

--- frontend/src/components/DeleteConfirmationModal.jsx
+++ frontend/src/components/DeleteConfirmationModal.jsx
@@ -21,18 +21,21 @@ function DeleteConfirmationModal({ note, onDelete, onClose }) {
         <div className="confirm-modal-overlay">
             <div className="confirm-modal-content">
                 <h3>Are you sure you want to delete this note?</h3>
-                <button 
-                    className="delete-modal-btn"
-                    onClick={handleLoading}
-                    disabled={loading}
-                    >
-                    {loading ? "Deleting" : "Yes"}
-                </button>
-                <button className="delete-modal-btn"
-                    onClick={onClose}
-                    disabled={loading}
-                    >No
-                </button>
+                <div className="confirm-btns">
+                    <button 
+                        className="confirm-modal-btn"
+                        onClick={handleLoading}
+                        disabled={loading}
+                        >
+                        {loading ? "Deleting" : "Yes"}
+                    </button>
+                    <button 
+                        className="confirm-modal-btn"
+                        onClick={onClose}
+                        disabled={loading}
+                        >No
+                    </button>
+                </div>
             </div>
         </div>
     )
