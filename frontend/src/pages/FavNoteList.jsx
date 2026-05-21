d22030b96de421b144dc6b57795df2f1bbf75d37
jjackson112 <jjackson11211@gmail.com>
2026-05-21 11:09:03 -0400

    create empty array for favNotes + pass FavoriteContext shared favorite state

--- frontend/src/pages/FavNoteList.jsx
+++ frontend/src/pages/FavNoteList.jsx
@@ -1,10 +1,13 @@
 import { api } from "../api/api";
 import Header from "../components/Header";
 import NoteCard from "../components/NoteCard";
+import { useFavorites } from "../context/FavoritesContext";
 
 // GET favorite ids list + filter?
 
 function FavNoteList() {
+    const [favNotes, setFavNotes] = useState([])
+    const {favorites, toggleFavorite } = useFavorites()
 
     const favoriteNotes = note.filter(note => favoriteNotes.includes(note.id))
     
