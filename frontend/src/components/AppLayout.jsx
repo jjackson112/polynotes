b8b55341abd6a2459ad83a7a1944bb827c4dc6ae
jjackson112 <jjackson11211@gmail.com>
2026-05-24 13:43:41 -0400

    consider using Outlet to prevent duplication, reuse layouts, nest route trees + persistence

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 
 {/* What authenticated users should see - header + sidebar */}
 function AppLayout({ children, sidebarOpen, setSidebarOpen, authMessage }) {
