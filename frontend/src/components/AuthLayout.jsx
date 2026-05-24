ce7def4935f41d58a9ef034900c9f52b2c2c68bc
jjackson112 <jjackson11211@gmail.com>
2026-05-24 14:45:04 -0400

    Outlet is modern style - children is old

--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function AuthLayout({ children }) {
     return (
         <div className="auth-shell">
-            {children}
+            <Outlet />
         </div>
     )
 }
