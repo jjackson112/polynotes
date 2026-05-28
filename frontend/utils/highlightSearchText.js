3bae2f51c917d10f37d66e7855abf093df5f3904
jjackson112 <jjackson11211@gmail.com>
2026-05-28 12:38:03 -0400

    regex - dynamic search pattern + return all matches

--- frontend/utils/highlightSearchText.js
+++ frontend/utils/highlightSearchText.js
@@ -1,5 +1,10 @@
 export function HighlightSearchText(text, searchTerm) {
     if (!searchTerm || !text) return text
+
+    // build a search pattern dynamically - regex
+    // returns all matches despite capitalization (g-global, i-case insensitive)
+    const regex = new RegExp(`(${SearchTerm})`, "gi")
+    
 }
 
 export default HighlightSearchText;
