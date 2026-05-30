90cbda907fc1fa8ba55b214a2cd96d3d962ab4cb
jjackson112 <jjackson11211@gmail.com>
2026-05-30 19:38:37 -0400

    more CSS for language page

--- frontend/src/pages/LanguageList.jsx
+++ frontend/src/pages/LanguageList.jsx
@@ -4,11 +4,16 @@ function LanguageList() {
     const languages = ["English", "Hawaiian", "Italian", "Mandarin", "Spanish"]
 
     return (
-        
-        <div className="language-list">
-            {languages.map((language) => (
-                <LanguageCard key={language} language={language} count={0} />
-            ))}
+        <div className="language-page">
+            <div className="language-title">
+                <h2>Languages</h2>
+            </div>
+
+            <div className="language-list">
+                {languages.map((language) => (
+                    <LanguageCard key={language} language={language} count={0} />
+                ))}
+            </div>
         </div>
     )
 }
