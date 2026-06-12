c5e877893b27fd1c96fe1b40ff0ed7ae401d7762
jjackson112 <jjackson11211@gmail.com>
2026-06-12 19:57:29 -0400

    beginning of Tag page + flask route to count

---
+++ frontend/src/pages/TagList.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import { api } from "../api/api";
+
+// envision a tag cloud - how does frontend know how big the tag cloud gets for each tag?
+// new Flask route? - keep count of how many notes belong to each tag
+
