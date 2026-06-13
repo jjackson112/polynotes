17d8986a38907559ae6c3a51b009cc177e65357a
jjackson112 <jjackson11211@gmail.com>
2026-06-12 21:58:45 -0400

    note about Object.entries(tags) in TagList

--- frontend/src/pages/TagList.jsx
+++ frontend/src/pages/TagList.jsx
@@ -1,14 +1,35 @@
 import { useState, useEffect } from "react";
 import { api } from "../api/api";
+import TagBubble from "../components/TagBubble";
 
 function TagList() {
-    const [tag, setTag] = useState([])
+    const [tags, setTags] = useState({}) // tuples, not array {}
 
     useEffect(() => {
         api.get("/notes/tag-counts")
-        .then(data => setTag(data))
+        .then(data => setTags(data))
         .catch(err => console.error(err))
     }, [])
+
+    // Object.entries(tags) converts tags into key-value pairs { grammar: 4, poetry: 2 } into [["grammar", 4], ["poetry", 2]] so .map() can occur
+
+    return (
+        <div className="tag-page">
+            <div className="tag-title">
+                <h2>Tags</h2>
+            </div>
+
+            <div className="tag-cloud">
+                {Object.entries(tags).map(([tag, count]) => {
+                    <TagBubble
+                        key={tag}
+                        tag={tag}
+                        count={count}
+                    />
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default TagList;
