b5b8f466da8b1201732e8eaed13904d0f87a636c
jjackson112 <jjackson11211@gmail.com>
2026-06-12 22:19:00 -0400

    pass tag to TagBubble

--- frontend/src/components/TagBubble.jsx
+++ frontend/src/components/TagBubble.jsx
@@ -4,7 +4,7 @@ import { Hash } from "react-feather";
 // envision a tag cloud - how does frontend know how big the tag cloud gets for each tag?
 // new Flask route? - keep count of how many notes belong to each tag
 
-function TagBubble({ count }) {
+function TagBubble({ tag, count = 0 }) {
     const tagSize = (count) => {
         if (count >= 10) return "tag-bubble-xl"
         if (count >= 5) return "tag-bubble-lg"
@@ -14,10 +14,10 @@ function TagBubble({ count }) {
 
     return (
         <Link 
-            to={`/notes?tag=${tag}`} 
+            to={`/notes?tag=${tag}&page=1`} 
             className={`tag-bubble ${tagSize(count)}`}
         >
-            <span><Hash/></span>
+            <span><Hash size="14" />{tag}</span>
             <p>{count}</p>
         </Link>
     )
