4f104cdb7e60f31ccf247aae3ab65f35ff6e1aaa
jjackson112 <jjackson11211@gmail.com>
2026-06-12 21:45:34 -0400

    fetch tags in TagList

--- frontend/src/components/TagBubble.jsx
+++ frontend/src/components/TagBubble.jsx
@@ -4,7 +4,7 @@ import { Hash } from "react-feather";
 // envision a tag cloud - how does frontend know how big the tag cloud gets for each tag?
 // new Flask route? - keep count of how many notes belong to each tag
 
-function TagBubble() {
+function TagBubble({ count }) {
     const tagSize = (count) => {
         if (count >= 10) return "tag-bubble-xl"
         if (count >= 5) return "tag-bubble-lg"
@@ -13,7 +13,10 @@ function TagBubble() {
     }
 
     return (
-        <Link to={`/notes?tag=${tag}`} className={`tag-bubble ${getTagSize(count)}`}>
+        <Link 
+            to={`/notes?tag=${tag}`} 
+            className={`tag-bubble ${tagSize(count)}`}
+        >
             <span><Hash/></span>
             <p>{count}</p>
         </Link>
--- frontend/src/pages/TagList.jsx
+++ frontend/src/pages/TagList.jsx
@@ -1,2 +1,14 @@
 import { useState, useEffect } from "react";
 import { api } from "../api/api";
+
+function TagList() {
+    const [tag, setTag] = useState([])
+
+    useEffect(() => {
+        api.get("/notes/tag-counts")
+        .then(data => setTag(data))
+        .catch(err => console.error(err))
+    }, [])
+}
+
+export default TagList;
