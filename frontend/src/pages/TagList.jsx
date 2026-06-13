ed61cc6029b4a9c6ab4cd60162f6690d0f20175f
jjackson112 <jjackson11211@gmail.com>
2026-06-12 20:56:02 -0400

    add GET route for tags + frontend function for count

--- frontend/src/pages/TagList.jsx
+++ frontend/src/pages/TagList.jsx
@@ -4,3 +4,9 @@ import { api } from "../api/api";
 // envision a tag cloud - how does frontend know how big the tag cloud gets for each tag?
 // new Flask route? - keep count of how many notes belong to each tag
 
+const tagSize = (count) => {
+    if (count >= 10) return "tag-bubble-xl"
+    if (count >= 5) return "tag-bubble-lg"
+    if (count >= 2) return "tag-bubble-md"
+    return "tag-bubble-sm"
+}
